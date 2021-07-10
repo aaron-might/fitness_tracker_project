26fbaaf6081e79bce42e8d7c89fb50ddce53a89f
aaron-might <aaronmighty7@gmail.com>
2021-07-10 00:12:51 -0400

    creating index.js and write workout schema

---
+++ Develop/models/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day:{
+        type: Date,
+        trim: true,
+    }, 
+
+    exercices:[{ 
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number
+    }]
+
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema )
+module.exports = Workout
